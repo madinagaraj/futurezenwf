e7edc27a32d88e6828256a88e82d554d4cc8dc64
Nagaraj Madi <madinagaraj@gmail.com>
2024-03-18 12:34:23 +0530

    URL and other changes

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from pydub import AudioSegment
 import tempfile
 import os
 from fastapi.middleware.cors import CORSMiddleware
-import io
+import time
+
 
 import librosa
 import numpy as np
@@ -30,16 +31,26 @@ def extract_features(file_path):
         except Exception as e:
             print("Error encountered while parsing file:", file_path)
             return None
+        
+@app.post("/voice/analyse/ping")
+async def healthcheck():
+     respose ={
+          "Health Check Sucessful"
+     }
+     return respose
 
-@app.post("/process_voice/")
-async def process_voice(upload_file: UploadFile = File(...)):
-    # Save the uploaded voice sample to a temporary file
+@app.post("/voice/analyse")
+async def process_voice(sample: UploadFile = File(...)):
+   
     with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as temp_file:
         temp_filename = temp_file.name
-        temp_file.write(await upload_file.read())
+        temp_file.write(await sample.read())
 
-        
-  
+    start_time = time.time()    
+    status="false"
+    detectedVoice="false"
+    emotionalTone="unableToDetect"
+    backgroundNoiseLevel="unableToDetect"
     audio1 = AudioSegment.from_wav(temp_filename)
     duration = len(audio1) / 1000  # Duration in seconds
 
@@ -49,25 +60,43 @@ async def process_voice(upload_file: UploadFile = File(...)):
     
     if mfccs is not None:
           prediction = loaded_model.predict([mfccs])
+          status="success"
+          detectedVoice="true"
           if prediction[0] == 1 :
-             isHumanVoice= "true"
-             isFabicatedVoice= "false"
+              voiceType = "Human"
+              aiProbability = "0"
+              humanProbability= "100"
+
           else :
-             isHumanVoice= "false"
-             isFabicatedVoice= "true"
-               
+             voiceType = "Artificail"
+             aiProbability = "100"
+             humanProbability= "0" 
+                        
     else:
           return "Error extracting features from the example file."
     
-    # Generate JSON response
+    responseTime= time.time()- start_time 
+    
+   
     response = {
-        "isHumanVoice": isHumanVoice,
-        "isFabicatedVoice":isFabicatedVoice,
-        "duration_seconds": duration,
-        "sample_rate": audio1.frame_rate,
-        "channels": audio1.channels,
-        "file_size_bytes": os.path.getsize(temp_filename)
-    }
+         "status": status,              
+          "analysis":{
+                     "detectedVoice": detectedVoice,
+                     "voiceType" : voiceType,
+                     "confidenceScore": {
+                          "aiProbability": aiProbability,
+                          "humanProbability": humanProbability
+                                        },
+                     
+                     "additionalInfo": {
+                     "emotionalTone": emotionalTone,
+                     "backgroundNoiseLevel": backgroundNoiseLevel
+                            }
+                     },
+          "responseTime": responseTime
+ 
+                }
+   
 
     # Clean up the temporary file
     os.unlink(temp_filename)
